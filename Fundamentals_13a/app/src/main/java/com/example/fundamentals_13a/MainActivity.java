f5e6e346de483825a47d5c66fd469c6d5d16c811
lipollis <aline.pollis@hotmail.com>
2022-09-28 14:13:17 -0300

    Scrolling text

---
+++ Fundamentals_13a/app/src/main/java/com/example/fundamentals_13a/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fundamentals_13a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
